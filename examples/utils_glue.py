2eef25cef50e7e14c5ea7a4c182157b350565537
alexanderblnf <alexandru@g2g-online.nl>
2019-09-06 09:03:59 +0200

    Added support for evaluation for mantis_intents

--- examples/utils_glue.py
+++ examples/utils_glue.py
@@ -604,6 +604,8 @@ def compute_metrics(task_name, preds, labels):
         return {"acc": simple_accuracy(preds, labels)}
     elif task_name == "wnli":
         return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "mantis_intent":
+        return {"acc": simple_accuracy(preds, labels)}
     else:
         raise KeyError(task_name)
 
